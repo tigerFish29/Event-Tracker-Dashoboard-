0ed4c563ba02f4ba46a91adfb80e5ac8a8046afc
tigerFish29 <paulwchabz28@gmail.com>
2022-08-09 18:17:28 +0100

    Made changes to service class corrected mistakes in the code

--- src/main/java/com/eventtracker/events/service/UsersService.java
+++ src/main/java/com/eventtracker/events/service/UsersService.java
@@ -1,44 +1,69 @@
 package com.eventtracker.events.service;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.eventtracker.events.domain.Users;
 import com.eventtracker.events.model.UsersDTO;
 import com.eventtracker.events.repos.UserRepository;
 
-
 @Service
 public class UsersService {
 
-    private final UserRepository userRepository;
-
-    private ModelMapper mapper;
-
-    private UsersDTO mapToDTO(Users users) {
-        return mapper.map(users, UsersDTO.class);
-    }
+    private final UserRepository usersRepository;
 
     // constructor 
-    @Autowired
-    public UsersService(UserRepository userRepository, ModelMapper mapper) {
-        super();
-        this.userRepository = userRepository;
-        this.mapper = mapper;
+    public UsersService(final UserRepository usersRepository) {
+        this.usersRepository = usersRepository;
     }
 
-
-    public Users addUser(Users users) {
-        return this.userRepository.save(users);
-    }
-
-    // find all users 
     public List<UsersDTO> findAll() {
-        return userRepository.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+        return usersRepository.findAll().stream()
+        .map(users -> mapToDTO(users, new UsersDTO())).collect(Collectors.toList());
+
     }
-   
+
+    // get 
+
+    public UsersDTO get(final Long id) {
+        return usersRepository.findById(id)
+                .map(users -> mapToDTO(users, new UsersDTO()))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    // create 
+
+    public Long create(final UsersDTO usersDTO) {
+        final Users users = new Users();
+        mapToEntity(usersDTO, users);
+        return usersRepository.save(users).getId();
+
+    }
+
+    // update 
+    public void update(final Long id, final UsersDTO usersDTO) {
+        final Users users = usersRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        mapToEntity(usersDTO, users);
+        usersRepository.save(users);
+    }
+
+    // delete method 
+    public void delete(final Long id) {
+        usersRepository.deleteById(id);
+    }
+    
+
+    private void mapToEntity(UsersDTO usersDTO, Users users) {
+    }
+
+    private UsersDTO mapToDTO(Users users, UsersDTO usersDTO) {
+        return null;
+    }
     
 }

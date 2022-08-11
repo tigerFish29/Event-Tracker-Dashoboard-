ed3375dbe0e238794a083dbd1f295a37141db4ed
tigerFish29 <paulwchabz28@gmail.com>
2022-08-11 15:56:18 +0100

    minor changes

--- src/main/java/com/eventtracker/events/controller/CollectionEventsController.java
+++ src/main/java/com/eventtracker/events/controller/CollectionEventsController.java
@@ -1,10 +1,15 @@
 package com.eventtracker.events.controller;
 
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.eventtracker.events.domain.CollectionEvents;
 import com.eventtracker.events.service.CollectionEventsService;
@@ -40,7 +45,33 @@ public class CollectionEventsController {
         } 
     
     // create a collection {}  
-    
+    @RequestMapping(path = "/create", method = RequestMethod.POST)
+    public RedirectView createCollection(RedirectAttributes redirectAttributes, @ModelAttribute CollectionEvents collectionEvents) {
+        collectionEventsService.create(collectionEvents);
+        RedirectView redirectView = new RedirectView("/", true);
+        String message = "Created! ";
+        redirectAttributes.addFlashAttribute("collectionsUpdated",message);
+        return redirectView;
+    }
+
+    // update { collections }
+    @RequestMapping(path = "/update", method = RequestMethod.POST)
+    public RedirectView updateCollection(RedirectAttributes redirectAttributes, @PathVariable("id") Long id, @ModelAttribute CollectionEvents collectionEvents) {
+        collectionEventsService.update(id, collectionEvents);
+        RedirectView redirectedView = new RedirectView("/", true);
+        String message = "<b>Update sucess! </b>";
+        redirectAttributes.addFlashAttribute("collections-updated", message);
+        return redirectedView;
+    }
+
+    // delete {} 
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable final Long id, final RedirectAttributes redirectAttributes) throws NotFoundException {
+         collectionEventsService.delete(id);
+         String message = "<b>Deleted!</b>";
+         redirectAttributes.addFlashAttribute("message", message);
+         return "hello";
+    }
 
 
 

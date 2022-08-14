8500d3378c361e3f59b8610f75b0e1ab05cfa66f
tigerFish29 <paulwchabz28@gmail.com>
2022-08-14 23:58:32 +0100

    completed the employee service class

--- src/main/java/com/eventtracker/events/Repository/EmployeeRepo.java
+++ src/main/java/com/eventtracker/events/Repository/EmployeeRepo.java
@@ -1,5 +1,7 @@
 package com.eventtracker.events.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.eventtracker.events.model.Employee;
 
 @Repository
 public interface EmployeeRepo extends JpaRepository <Employee, Integer> {
-    
+    Optional<Employee> findByIdAndActive(Integer id, boolean active);
 }

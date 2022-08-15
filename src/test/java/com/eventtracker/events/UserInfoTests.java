603c756688b1df21e070e0782268559bc377e8ab
tigerFish29 <paulwchabz28@gmail.com>
2022-08-15 15:28:14 +0100

    made changes to test

--- src/test/java/com/eventtracker/events/UserInfoTests.java
+++ src/test/java/com/eventtracker/events/UserInfoTests.java
@@ -1,17 +1,11 @@
 package com.eventtracker.events;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-
-
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
-
 import com.eventtracker.events.Repository.UserRepository;
 import com.eventtracker.events.Service.UserService;
 import com.eventtracker.events.model.UserInfo;
